60f9d6e586cb298166d7271ce8ba8a9f62974b0e
Max Ma <maxma2@illinois.edu>
2024-01-23 13:56:45 -0600

    UVM Skeleton

---
+++ src/sim/core_sequence.svh
@@ -0,0 +1,25 @@
+// Sequence Items for Test Stimulus of MNIST Accelerator Core
+`define IMG_WIDTH 28
+`define IMG_HEIGHT 28
+
+class core_img_item extends uvm_sequence_item;
+    `uvm_object_utils(core_img_item)
+
+    rand bit [7:0] img [`IMG_WIDTH * `IMG_HEIGHT];
+
+    function new(string name);
+        super.new(name);
+    endfunction : new
+
+    virtual function void do_copy(uvm_object rhs);
+    endfunction : do_copy
+
+    virtual function void do_compare(uvm_object rhs, uvm_comparer comparer);
+    endfunction : do_compare
+
+    virtual function string convert2string();
+    endfunction : convert2string
+
+    virtual function void load_image(string file_name);
+    endfunction : load_image
+endclass : core_img_item
