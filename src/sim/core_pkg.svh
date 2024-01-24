a8b12a2e3400fddeef07ae4933f1752e1c7015ce
Max Ma <maxma2@illinois.edu>
2024-01-23 22:04:56 -0600

    UVM Classes Skeleton

--- src/sim/core_pkg.svh
+++ src/sim/core_pkg.svh
@@ -1,8 +1,12 @@
 // Top Package for MNIST Accelerator Core
+`include "core_interface.svh"
+
 package core_pkg;
     import uvm_pkg::*;
     `include "uvm_macros.svh"
 
-    
-    
+    `include "core_sequence.svh"
+    `include "core_agent.svh"
+    `include "core_env.svh"
+    `include "core_test.svh"
 endpackage
--- src/sim/core_sequence.svh
+++ src/sim/core_sequence.svh
@@ -1,20 +1,21 @@
 // Sequence Items for Test Stimulus of MNIST Accelerator Core
 `define IMG_WIDTH 28
-`define IMG_HEIGHT 28
+`define IMG_HEIGHT 28  
 
 class core_img_item extends uvm_sequence_item;
     `uvm_object_utils(core_img_item)
 
     rand bit [7:0] img [`IMG_WIDTH * `IMG_HEIGHT];
+    rand bit [3:0] label;
 
-    function new(string name);
+    function new(string name = "");
         super.new(name);
     endfunction : new
 
     virtual function void do_copy(uvm_object rhs);
     endfunction : do_copy
 
-    virtual function void do_compare(uvm_object rhs, uvm_comparer comparer);
+    virtual function bit do_compare(uvm_object rhs, uvm_comparer comparer);
     endfunction : do_compare
 
     virtual function string convert2string();
