7882df8c694a39b17ac6254d9b6caacc4bfca2b7
Max Ma <maxma2@illinois.edu>
2024-01-28 10:58:40 -0600

    Improved UVM Testbench, Needs Optimization

--- src/sim/core_test.svh
+++ src/sim/core_test.svh
@@ -4,6 +4,7 @@ class core_test extends uvm_test;
 
     // Environment, Sequence
     core_env env;
+    core_sequence seq;
 
     function new(string name, uvm_component parent);
         super.new(name, parent);
@@ -14,10 +15,15 @@ class core_test extends uvm_test;
         super.build_phase(phase);
         // Create Environment
         env = core_env::type_id::create("env", this);
+        seq = core_sequence::type_id::create("seq", this);
     endfunction : build_phase
 
     // Start Sequence
     task run_phase(uvm_phase phase);
+        // Run Test Sequence
+        phase.raise_objection(this);
         `uvm_info("TEST", "Running MNIST Accelerator Core Test", UVM_MEDIUM);
+        seq.start(env.agent.sequencer);
+        phase.drop_objection(this);
     endtask : run_phase
 endclass
